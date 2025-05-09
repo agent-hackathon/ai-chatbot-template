3bc382be3ddec17a47ea8be70f52ae5ac8f8cae6
Ådne S Djuve <aadne@gture.com>
2025-05-09 09:22:24 +0200

    Update auth configuration to remove domain setting in production and add PKCE code verifier options

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -51,7 +51,17 @@ export const authConfig = {
         sameSite: 'lax',
         path: '/',
         secure: process.env.NODE_ENV !=='development',
-        domain: process.env.NODE_ENV === 'development' ? undefined : '.d2gzk5ozyqicbv.amplifyapp.com',
+        domain: process.env.NODE_ENV === 'development' ? undefined : undefined,
+      },
+    },
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV !== 'development',
+        maxAge: 900,
       },
     },
   },
